dff9aace6576f864272d788720abd8fdb189d090
Peter Foelsche <uhcu_63@outlook.com>
2025-03-05 03:53:44 -0600

    added additional unary functions

--- unary.h
+++ unary.h
@@ -1,7 +1,15 @@
 __MAKE_ENTRY__(sqrt) __COMMA__
+__MAKE_ENTRY__(abs) __COMMA__
 __MAKE_ENTRY__(exp) __COMMA__
+__MAKE_ENTRY__(log) __COMMA__
+__MAKE_ENTRY__(log10) __COMMA__
 __MAKE_ENTRY__(sin) __COMMA__
 __MAKE_ENTRY__(cos) __COMMA2__
+__MAKE_ENTRY2__(erf) __COMMA2__
+__MAKE_ENTRY2__(erfc) __COMMA2__
+__MAKE_ENTRY2__(tgamma) __COMMA2__
+__MAKE_ENTRY2__(lgamma) __COMMA2__
+__MAKE_ENTRY2__(cbrt) __COMMA2__
 __MAKE_ENTRY2__(tan) __COMMA2__
 __MAKE_ENTRY2__(asin) __COMMA2__
 __MAKE_ENTRY2__(acos) __COMMA2__
