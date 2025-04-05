6d762aeca69b4f7eea65367a88771c2622b4a6d8
Peter Foelsche <uhcu_63@outlook.com>
2025-04-05 13:51:37 -0500

    protected unique::getSet() with a mutex

--- unique.h
+++ unique.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <memory>
 #include <set>
+#include <mutex>
 
 namespace theExpressionEngine
 {
@@ -16,20 +17,26 @@ struct unique:T
 	{
 	}
 	~unique(void)
-	{	getSet().erase(this);
+	{	std::lock_guard<std::recursive_mutex> sLock(getMutex());
+		getSet().erase(this);
 	}
 	struct compare
 	{	bool operator()(const unique*const _p0, const unique*const _p1) const
 		{	return *_p0 < *_p1;
 		}
 	};
+	static std::recursive_mutex &getMutex(void)
+	{	static std::recursive_mutex s;
+		return s;
+	}
 	static std::set<const unique<T>*, compare> &getSet(void)
 	{	static std::set<const unique<T>*, compare> s_sSet;
 		return s_sSet;
 	}
 	template<typename ...REST>
 	static typename T::ptr create(const factory&_rF, REST&&..._r)
-	{	return (*getSet().insert(
+	{	std::lock_guard<std::recursive_mutex> sLock(getMutex());
+		return (*getSet().insert(
 			static_cast<const unique<T>*>(collapse(*std::make_shared<const unique<T> >(std::forward<REST>(_r)...), _rF).get())
 		).first)->shared_from_this();
 	}
