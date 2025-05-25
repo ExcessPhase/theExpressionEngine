b1f1562400cedf4e35d42d367845c8a9efc563de
Peter Foelsche <uhcu_63@outlook.com>
2025-05-25 11:54:13 -0500

    removed commented-out code

--- unique.h
+++ unique.h
@@ -14,7 +14,6 @@ struct realConstant;
 struct factory;
 struct expression;
 boost::intrusive_ptr<const expression> collapse(const expression&, const factory&);
-#if 1
 	/// a nullmutex in case of a single threaded environment
 class NullMutex
 {	public:
@@ -92,51 +91,4 @@ class unique
 	}
 };
 
-#else
-template<typename T>
-struct unique:T
-{	template<typename ...REST>
-	unique(REST&&..._r)
-		:T(std::forward<REST>(_r)...)
-	{	//std::cout << typeid(*this).name() << ":" << m_iId << std::endl;
-	}
-	unique(void) = delete;
-	unique(const unique&) = delete;
-	unique(unique&&) = delete;
-	unique&operator=(const unique&) = delete;
-	unique&operator=(unique&&) = delete;
-	virtual ~unique(void)
-	{	//std::cout << "start of ~" << typeid(*this).name() << ":" << m_iId << std::endl;
-		std::lock_guard<std::recursive_mutex> sLock(getMutex());
-		auto &rSet = getSet();
-		const auto pFind = rSet.find(this);
-		if (pFind != rSet.end() && *pFind == this)
-			rSet.erase(pFind);
-		//std::cout << "end of ~" << typeid(*this).name() << ":" << m_iId << std::endl;
-	}
-	struct compare
-	{	bool operator()(const unique<T>*const _p0, const unique<T>*const _p1) const
-		{	return *_p0 < *_p1;
-		}
-	};
-	static auto &getMutex(void)
-	{	static std::recursive_mutex s;
-		return s;
-	}
-	static auto &getSet(void)
-	{	static std::set<const unique<T>*, compare> s_sSet;
-		return s_sSet;
-	}
-	template<typename ...REST>
-	static typename T::ptr _create(const factory&_rF, REST&&..._r)
-	{	const auto s = std::make_shared<const unique<T> >(std::forward<REST>(_r)...);
-		std::lock_guard<std::recursive_mutex> sLock(getMutex());
-		return (*getSet().insert(s.get()).first)->shared_from_this();
-	}
-	template<typename ...REST>
-	static typename T::ptr create(const factory&_rF, REST&&..._r)
-	{	return collapse(*_create(_rF, std::forward<REST>(_r)...), _rF);
-	}
-};
-#endif
 }
